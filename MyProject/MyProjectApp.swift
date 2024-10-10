01b8a77368ec2773cde8d00a3f3492779667a817
Голубенко М. И <golubenko.mi39@gmail.com>
2024-10-10 08:48:59 +0700

    Initial Commit

---
+++ MyProject/MyProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyProjectApp.swift
+//  MyProject
+//
+//  Created by user on 10.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct MyProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
